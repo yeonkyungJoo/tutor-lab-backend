c21d515a5f071c437afcbc4cb3f5d6f603fd90fc
최성준 <csj2702@naver.com>
2021-07-03 14:31:22 +0900

    init

---
+++ src/main/java/com/tutor/tutorlab/TutorlabApplication.java
@@ -0,0 +1,13 @@
+package com.tutor.tutorlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutor/tutorlab/TutorlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutor.tutorlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
