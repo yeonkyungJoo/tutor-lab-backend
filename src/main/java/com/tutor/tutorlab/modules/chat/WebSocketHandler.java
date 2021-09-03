e3985e9eaa1429730fa7af92df098b086f5a6064
yeonkyung <younkyong94@gmail.com>
2021-09-03 21:12:27 +0900

    WebSocketHandler 변경

--- src/main/java/com/tutor/tutorlab/modules/chat/WebSocketHandler.java
+++ src/main/java/com/tutor/tutorlab/modules/chat/WebSocketHandler.java
@@ -3,6 +3,7 @@ package com.tutor.tutorlab.modules.chat;
 import com.tutor.tutorlab.modules.chat.enums.MessageType;
 import com.tutor.tutorlab.modules.chat.repository.ChatroomRepository;
 import com.tutor.tutorlab.modules.chat.service.MessageService;
+import com.tutor.tutorlab.modules.chat.vo.Chatroom;
 import com.tutor.tutorlab.modules.chat.vo.Message;
 import com.tutor.tutorlab.utils.JsonUtil;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -31,14 +33,15 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @PostConstruct
     private void init() {
-//        List<Chatroom> chatrooms = chatroomRepository.findAll();
-//        chatrooms.stream().forEach(chatroom -> {
-//            chatroomMap.put(chatroom.getId(), new HashMap<>());
-//        });
 
-        chatroomMap.put(1L, new HashMap<>());
-        chatroomMap.put(2L, new HashMap<>());
-        chatroomMap.put(3L, new HashMap<>());
+        List<Chatroom> chatrooms = chatroomRepository.findAll();
+        chatrooms.stream().forEach(chatroom -> {
+            chatroomMap.put(chatroom.getId(), new HashMap<>());
+        });
+
+//        chatroomMap.put(1L, new HashMap<>());
+//        chatroomMap.put(2L, new HashMap<>());
+//        chatroomMap.put(3L, new HashMap<>());
     }
 
     // 소켓 연결
