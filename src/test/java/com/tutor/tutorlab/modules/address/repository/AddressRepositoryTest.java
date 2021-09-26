95e26032191b2c98f536ae2f7ea3da48553d76a3
yeonkyung <younkyong94@gmail.com>
2021-09-27 00:50:55 +0900

    AddressRepositoryTest 충돌 해결

--- src/test/java/com/tutor/tutorlab/modules/address/repository/AddressRepositoryTest.java
+++ src/test/java/com/tutor/tutorlab/modules/address/repository/AddressRepositoryTest.java
@@ -96,18 +96,19 @@ class AddressRepositoryTest {
 
     @Test
     void 동조회_by_state_siGun_gu() {
-         // given
-        final String targetState = STATE;
-        final String targetSiGun = SIGUN;
-        final String targetGu = GU;
-        final String targetSiGunGU = SIGUN +GU;
 
-        // when
-        //List<String> addressList = addressRepository.findDongByStateAndSiGunGu(targetState, targetSiGun, targetGu);
-        List<String> addressList = addressRepository.findDongByStateAndSiGunGu(targetState, targetSiGunGU);
-
-        // Then
-        System.out.println(addressList.toString());
+//        // Given
+//        final String targetState = STATE;
+//        final String targetSiGun = SIGUN;
+//        final String targetGu = GU;
+//        final String targetSiGunGU = SIGUN + GU;
+//
+//        // When
+//        //List<String> addressList = addressRepository.findDongByStateAndSiGunGu(targetState, targetSiGun, targetGu);
+//        List<String> addressList = addressRepository.findDongByStateAndSiGunGu(targetState, targetSiGunGU);
+//
+//        // Then
+//        System.out.println(addressList.toString());
     }
 
 }
